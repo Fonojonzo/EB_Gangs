17208c9da55015be3846fb5a36353db443fc3793
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-12-21 00:19:58 -0300

    Add files via upload

---
+++ eb.lua
@@ -0,0 +1,19 @@
+local Gangs = {
+    [32325896] = {
+        Nome = 'Tokyo Manji Gang',
+        Vida = 2000,
+        Itens = {},
+        Roupa = {Tier = 95,Shirt = '',Pants = ''},
+        Cor = {
+            Nome = {Letra = Color3.FromRGB(0,0,0),Borda = Color3.FromRGB(0,0,0)},
+            Patente = {Letra = Color3.FromRGB(0,0,0),Borda = Color3.FromRGB(0,0,0)},
+            Team = {Letra = Color3.FromRGB(0,0,0),Borda = Color3.FromRGB(0,0,0)},
+        },
+    },
+
+
+    
+ 
+}
+
+return Gangs
