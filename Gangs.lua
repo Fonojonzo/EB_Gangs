eb27ff675d0e51f3cb94e118fa69bc9a2ea8183f
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-02-25 18:45:17 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1,265 +1,265 @@
 local Gangs = {
-		[32325896] = {
-			Nome = 'Tokyo Manji Gang',
-			Vida = 2000,
-			Itens = {['Dash'] = 0,['Mikey'] = 0,['Yamato'] = 30,['TacoLucille'] = 50,['Granada'] = 254},
-			Roupa = {Tier = 95,Shirt = 6850745796,Pants = 7166106431},
-			Cor = {
-				Nome = 	  {Letra = {0,0,0},Borda = {255,255,255}},
-				Patente = {Letra = {0,0,0},Borda = {255,255,255}},
-				Team =    {Letra = {0,0,0},Borda = {255,255,0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
 	
-		[32942292] = { 
-			Nome = 'Luas Superiores',
-			Vida = 2000,
-			Itens = {['PisoLuas'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {107, 37, 1}},
-				Team =    {Letra = {0, 0, 0},Borda = {107, 37, 1}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33838526] = { 
-			Nome = 'A BatFamilia',
-			Vida = 2000,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0}, Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {100, 0, 0}},
-			},
-		},--
-	
-	
-	
-		[33529431] = { -- Dono Satoru_Gojou021
-			Nome = 'The Kings',
-	
-			Vida = 750,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = {
-				Nome = 	  {Letra = {0,0,0},Borda = {255,255,5}},
-				Patente = {Letra = {0,0,0},Borda = {255,255,5}},
-				Team =    {Letra = {0,0,0},Borda = {255,255,5}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-	
-		[33524892] = { 
-			Nome = 'Brahman Gang',
-			Vida = 1000,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 260,Shirt = 7061606670,Pants = 8995167282},
-			Cor = {
-				Nome = 	  {Letra = {255, 255, 255},Borda = {0, 0, 0}},
-				Patente = {Letra = {0,0,0},Borda = {100, 0, 0}},
-				Team =    {Letra = {0,0,0},Borda = {100, 0, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33504780] = { 
-			Nome = 'Feiticeiros Jujutsu',
-			Vida = 800,
-			Itens = {['Mikey'] = 0,['TacoLucille'] = {255,254,100},['Dash'] = 0,['Yamato'] = 255},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = {
-				Nome = 	  {Letra = {255, 255, 255},Borda = {0, 0, 0}},
-				Patente = {Letra = {255, 255, 255},Borda = {0, 0, 0}},
-				Team =    {Letra = {0, 0, 0},Borda = {255,255,0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33483361] = { 
-			Nome = 'Jujutsu Gang',
-			Vida = 500,
-			Itens = {['Josef'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = {
-				Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {96, 0, 128}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33386384] = { 
-			Nome = 'The Gods of Legends',
-			Vida = 1000,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {2, 2, 2},Borda = {170, 85, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {170, 85, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {170, 85, 255}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33452794] = { 
-			Nome = 'White Dragons',
-			Vida = 500,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 254,Shirt = 10394217131,Pants = 3734685568},
-			Cor = { 
-				Nome = 	  {Letra = {255, 255, 255},Borda = {85, 0, 127}},
-				Patente = {Letra = {255, 255, 255},Borda = {85, 0, 127}},
-				Team =    {Letra = {255, 255, 255},Borda = {85, 0, 127}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33765458] = { -- 1 denuncia
-			Nome = 'The Sapphire Serpents',
-			Vida = 500,
-			Itens = {['Josef'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {0, 255, 0}},
-				Patente = {Letra = {0, 0, 0},Borda = {0, 255, 0}},
-				Team =    {Letra = {0, 0, 0},Borda = {0, 255, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-	
-		[33822704] = { -- The Gods Of Universe (grupo) - Loid e // Rafael (pagou)
-			Nome = 'The Cursed Sorcerers',
-			Vida = 500,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {153, 0, 0}},
-				Patente = {Letra = {0, 0, 0},Borda = {153, 0, 0}},
-				Team =    {Letra = {0, 0, 0},Borda = {153, 0, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33784506] = { 
-			Nome = 'Dragons Strategist',
-			Vida = 750,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {254,238,117},Borda = {0, 0, 0}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33784819] = { 
-			Nome = 'The Vortex Gang',
-			Vida = 500,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {67, 102, 244}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33765848] = { 
-			Nome = 'Void Gang 帮派',
-			Vida = 500,
-			Itens = {['Josef'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {255, 0, 0},Borda = {255, 255, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33803150] = { 
-			Nome = 'Yamaguchi Gumi',
-			Vida = 750,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 95,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 255},Borda = {0, 0, 0}},
-				Patente = {Letra = {0, 0, 255},Borda = {0, 0, 0}},
-				Team =    {Letra = {0, 0, 255},Borda = {0, 0, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-	
-		[33817647] = { 
-			Nome = 'Máfia da Passione',
-			Vida = 750,
-			Itens = {['Mikey'] = 0,['TacoLucille'] = 248,['Dash'] = 0,['Yamato'] = 200},
-			Roupa = {Tier = 255,Shirt = 7279742828,Pants = 5375274422},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {204, 0, 153}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 0}},
-				Team =    {Letra = {0, 0, 0},Borda = {255, 255, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-	
-		[33820952] = { 
-			Nome = 'Cosmic Thunders',
-			Vida = 750,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 255,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {0, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {0, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {0, 255, 255}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[11622572] = { 
-			Nome = 'The Amethyst',
-			Vida = 750,
-			Itens = {['Mikey'] = 0},
-			Roupa = {Tier = 255,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {153, 0, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {153, 0, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {153, 0, 255}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33866159] = { 
-			Nome = 'Shark Dragons',
-			Vida = 1250,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 255,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {255, 0, 0},Borda = {0, 255, 0}},
-				Patente = {Letra = {255, 0, 0},Borda = {0, 255, 0}},
-				Team =    {Letra = {255, 0, 0},Borda = {0, 255, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-	
-		[33405906] = { 
-			Nome = 'Moebius',
-			Vida = 750,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 255,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},Borda = {255, 0, 0}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 0, 0}},
-				Team =    {Letra = {0, 0, 0},Borda = {255, 0, 0}},
-			},
-		},----------------------------------- Gang ---------------------------------------------
-	
-		[33903684] = { 
-			Nome = 'Arcanjos',
-			Vida = 750,
-			Itens = {['Mikey'] = 0,['Dash'] = 0},
-			Roupa = {Tier = 255,Shirt = 0,Pants = 0},
-			Cor = { 
-				Nome = 	  {Letra = {0 , 0, 0},Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
-				Team =    {Letra = {0, 0, 0},Borda = {0, 153, 255}},
-				},
-		},----------------------------------- Gang ---------------------------------------------
+	[32325896] = {
+		Nome = 'Tokyo Manji Gang',
+		Vida = 2000,
+		Itens = {['Dash'] = 0,['Mikey'] = 0,['Yamato'] = 30,['TacoLucille'] = 50,['Granada'] = 254},
+		Roupa = {Tier = 95,Shirt = 6850745796,Pants = 7166106431},
+		Cor = {
+			Nome = 	  {Letra = {0,0,0},Borda = {255,255,255}},
+			Patente = {Letra = {0,0,0},Borda = {255,255,255}},
+			Team =    {Letra = {0,0,0},Borda = {255,255,0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
 
-	}
+	[32942292] = { 
+		Nome = 'Luas Superiores',
+		Vida = 2000,
+		Itens = {['PisoLuas'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {107, 37, 1}},
+			Team =    {Letra = {0, 0, 0},Borda = {107, 37, 1}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33838526] = { 
+		Nome = 'A BatFamilia',
+		Vida = 2000,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0}, Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {100, 0, 0}},
+		},
+	},--
+
+
+
+	[33529431] = { -- Dono Satoru_Gojou021
+		Nome = 'The Kings',
+
+		Vida = 750,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = {
+			Nome = 	  {Letra = {0,0,0},Borda = {255,255,5}},
+			Patente = {Letra = {0,0,0},Borda = {255,255,5}},
+			Team =    {Letra = {0,0,0},Borda = {255,255,5}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+
+	[33524892] = { 
+		Nome = 'Brahman Gang',
+		Vida = 1000,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 260,Shirt = 7061606670,Pants = 8995167282},
+		Cor = {
+			Nome = 	  {Letra = {255, 255, 255},Borda = {0, 0, 0}},
+			Patente = {Letra = {0,0,0},Borda = {100, 0, 0}},
+			Team =    {Letra = {0,0,0},Borda = {100, 0, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33504780] = { 
+		Nome = 'Feiticeiros Jujutsu',
+		Vida = 800,
+		Itens = {['Mikey'] = 0,['TacoLucille'] = {255,254,100},['Dash'] = 0,['Yamato'] = 255},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = {
+			Nome = 	  {Letra = {255, 255, 255},Borda = {0, 0, 0}},
+			Patente = {Letra = {255, 255, 255},Borda = {0, 0, 0}},
+			Team =    {Letra = {0, 0, 0},Borda = {255,255,0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33483361] = { 
+		Nome = 'Jujutsu Gang',
+		Vida = 500,
+		Itens = {['Josef'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = {
+			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {96, 0, 128}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33386384] = { 
+		Nome = 'The Gods of Legends',
+		Vida = 1000,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {2, 2, 2},Borda = {170, 85, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {170, 85, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {170, 85, 255}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33452794] = { 
+		Nome = 'White Dragons',
+		Vida = 500,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 254,Shirt = 10394217131,Pants = 3734685568},
+		Cor = { 
+			Nome = 	  {Letra = {255, 255, 255},Borda = {85, 0, 127}},
+			Patente = {Letra = {255, 255, 255},Borda = {85, 0, 127}},
+			Team =    {Letra = {255, 255, 255},Borda = {85, 0, 127}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33765458] = { -- 1 denuncia
+		Nome = 'The Sapphire Serpents',
+		Vida = 500,
+		Itens = {['Josef'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {0, 255, 0}},
+			Patente = {Letra = {0, 0, 0},Borda = {0, 255, 0}},
+			Team =    {Letra = {0, 0, 0},Borda = {0, 255, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+
+	[33822704] = { -- The Gods Of Universe (grupo) - Loid e // Rafael (pagou)
+		Nome = 'The Cursed Sorcerers',
+		Vida = 500,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {153, 0, 0}},
+			Patente = {Letra = {0, 0, 0},Borda = {153, 0, 0}},
+			Team =    {Letra = {0, 0, 0},Borda = {153, 0, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33784506] = { 
+		Nome = 'Dragons Strategist',
+		Vida = 750,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {254,238,117},Borda = {0, 0, 0}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33784819] = { 
+		Nome = 'The Vortex Gang',
+		Vida = 500,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {67, 102, 244}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33765848] = { 
+		Nome = 'Void Gang 帮派',
+		Vida = 500,
+		Itens = {['Josef'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {255, 0, 0},Borda = {255, 255, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33803150] = { 
+		Nome = 'Yamaguchi Gumi',
+		Vida = 750,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 255},Borda = {0, 0, 0}},
+			Patente = {Letra = {0, 0, 255},Borda = {0, 0, 0}},
+			Team =    {Letra = {0, 0, 255},Borda = {0, 0, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+
+	[33817647] = { 
+		Nome = 'Máfia da Passione',
+		Vida = 750,
+		Itens = {['Mikey'] = 0,['TacoLucille'] = 248,['Dash'] = 0,['Yamato'] = 200},
+		Roupa = {Tier = 255,Shirt = 7279742828,Pants = 5375274422},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {204, 0, 153}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 0}},
+			Team =    {Letra = {0, 0, 0},Borda = {255, 255, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+
+	[33820952] = { 
+		Nome = 'Cosmic Thunders',
+		Vida = 750,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {0, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {0, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {0, 255, 255}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[11622572] = { 
+		Nome = 'The Amethyst',
+		Vida = 750,
+		Itens = {['Mikey'] = 0},
+		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {153, 0, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {153, 0, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {153, 0, 255}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33866159] = { 
+		Nome = 'Shark Dragons',
+		Vida = 1250,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {255, 0, 0},Borda = {0, 255, 0}},
+			Patente = {Letra = {255, 0, 0},Borda = {0, 255, 0}},
+			Team =    {Letra = {255, 0, 0},Borda = {0, 255, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+
+	[33405906] = { 
+		Nome = 'Moebius',
+		Vida = 750,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 0, 0}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 0, 0}},
+			Team =    {Letra = {0, 0, 0},Borda = {255, 0, 0}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
+	[33903684] = { 
+		Nome = 'Arcanjos',
+		Vida = 750,
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
+		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
+		Cor = { 
+			Nome = 	  {Letra = {0 , 0, 0},Borda = {255, 255, 255}},
+			Patente = {Letra = {0, 0, 0},Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},Borda = {0, 153, 255}},
+		},
+	},----------------------------------- Gang ---------------------------------------------
+
 }
 
 return Gangs
