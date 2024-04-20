310d2ec15f4389f47f5ba95342dce887699a7bda
iExTno <132175289+iExTno@users.noreply.github.com>
2024-04-20 16:47:57 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -275,7 +275,7 @@ local Gangs = {
 	[33969792] = { 
 		Nome = 'Syndicato Cruor',
 		Vida = 750,
-		Itens = {['Mikey'] = 0},
+		Itens = {['Mikey'] = 0,['Dash'] = 0},
 		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0 , 0, 0},Borda = {255, 0, 0}},
