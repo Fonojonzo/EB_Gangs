63f228a1a34f599f33149ad4b5c5ac1bf08bacb7
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-04-26 14:31:14 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -346,9 +346,9 @@ local Gangs = {
 	},----------------------------------- Gang ---------------------------------------------
 
 	[34008520] = { 
-		Nome = "あshadow slayerぁ",
+		Nome = "あ Shadow Slayer ぁ",
 		Vida = 500,
-		Itens = {['Mikey'] = 0},
+		Itens = {['Mikey'] = 0,['Dash'] = 0,['TacoLucille'] = 0},
 		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {17, 17, 17},Borda = {255, 0, 0}},
