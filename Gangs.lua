3aadfede190f522ac2a31c992ef874cbdab9c415
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-05-24 18:06:41 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -382,7 +382,7 @@ local Gangs = {
 	[34223612] = { 
 		Nome = "キ Renegados The Gang キ",
 		Vida = 750,
-		Itens = {['Mikey'] = 0,['TacoLucille'] = 192},
+		Itens = {['KokusenKick'] = 0,['TacoLucille'] = 192,['Dash'] = 0},
 		Roupa = {Tier = 255,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
