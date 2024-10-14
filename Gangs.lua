ce97e09fee07960f82878c1f825e25a54cbc03c4
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-10-14 14:56:25 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -249,7 +249,7 @@ local Gangs = {
 	[34427514] = {   -- [Dono ID:4592829229 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡
 		Nome = "戒め 10 Mandamentos 戒め",
 		Vida = 500,
-		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0},
+		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0,['FireKick'] = 0},
 		Roupa = {Tier = 255,Shirt = 15808598116,Pants = 15815194659},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
