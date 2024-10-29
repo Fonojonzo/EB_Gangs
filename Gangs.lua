56148820ea0ec1e3d2d8c331d81e892107e80de0
iExTno <132175289+iExTno@users.noreply.github.com>
2024-10-29 12:00:13 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -3,7 +3,7 @@ local Gangs = {
 	[32942292] = { 
 		Nome = 'Luas Superiores',
 		Vida = 3000,
-		Itens = {['PisoLuas'] = 0,['Colateral'] = 0,['Dash'] = 0,['FP_Thunder'] = 0,['Bite'] = 0,['DoubleJump'] = 0},
+		Itens = {['PisoLuas'] = 0,['Colateral'] = 0,['Dash'] = 0,['FP_Thunder'] = 254,['Bite'] = 0,['DoubleJump'] = 0},
 		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
