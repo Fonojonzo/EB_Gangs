bc4fc78c426c8dd921e2e631b41517169177dace
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-12-02 03:20:28 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -74,7 +74,7 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[33504780] = {   -- [Dono ID:4035850120 ] slot 15
+	[35276401] = {   -- [Dono ID:7648333286 ] slot 15
 		Nome = 'ⅰ.ل',
 		Vida = 800,
 		Itens = {['KokusenKick'] = 0,['TacoLucille'] = {255,254,100},['ItachiDash'] = 0,['Yamato'] = 5,['FireKick'] = 0,['DoubleJump'] = 255,['Raio'] = {255, 254, 100},['YinYang'] = 0,['SusanooSlap'] = 0,['Rinnegan_Push'] = 0,['Hinokami_1'] = 0,['CorteDiabolico'] = 0},
