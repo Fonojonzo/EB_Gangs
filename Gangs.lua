d2973c2aa7098855605d7e5df0d8dd9e7ed7973f
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-01-13 12:38:28 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -77,13 +77,13 @@ local Gangs = {
 	},----------------------------------- Gang ---------------------------------------------
 
 	[35276401] = {   -- [Dono ID:7648333286 ] slot 15 (HP AUMENTADO)
-		Nome = 'Gotei 13',
+		Nome = 'Beginning of Disorder',
 		Vida = 1000,
 		Itens = {['SocoEletrico'] = 0,['TacoLucille'] = {255,254,100},['ItachiDash'] = 0,['Yamato'] = 5,['FireKick'] = 0,['DoubleJump'] = 255,['Raio'] = {255, 254, 100},['YinYang'] = 0,['SusanooSlap'] = 0,['Rinnegan_Push'] = 0,['Hinokami_1'] = 0,['CorteDiabolico'] = 0,['YinColateral'] = 0},
 		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
 		Cor = {
 			Nome = 	  {Letra = {255, 255, 255},Borda = {0,0,0}},
-			Patente = {Letra = {255, 255, 255},Borda = {0, 0, 0}},
+			Patente = {Letra = {0, 102, 102},Borda = {255, 255, 255}},
 			Team =    {Letra = {0, 0, 0},Borda = {255, 255, 255}},
 		},
 	},----------------------------------- Gang ---------------------------------------------
