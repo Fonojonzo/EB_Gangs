0a43dc1abd56776e90c3c750ff68d4ef2dbbae6e
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-01-17 16:32:01 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -893,10 +893,10 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[35407574] = {   -- [Dono ID: 5426598347] (HP AUMENTADO)
+	[35407574] = {   -- [Dono ID: 5426598347] (HP AUMENTADO) MAX 15 PLAYER
 		Nome = "Crimson Nebula",
 		Vida = 1500,
-		Itens = {['KokusenKick'] = 0,['ChuteCongelante'] = 0,['Raio'] = 0,['SusanooSlap'] = 0,['Meteoro'] = 0,['TacoLucille'] = 0,['Rinnegan_Push'] = 0,['MinatoKunai'] = 0,['F1VermelhoExplodir'] = 0,['Colateral'] = 0},
+		Itens = {['KokusenKick'] = 0,['ChuteCongelante'] = 0,['Raio'] = 0,['SusanooSlap'] = 0,['Meteoro'] = 0,['TacoLucille'] = 0,['Rinnegan_Push'] = 0,['MinatoKunai'] = 0,['F1VermelhoExplodir'] = 0,['Colateral'] = 0,['FP_Thunder'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {255, 255, 255},Borda = {0, 0, 255}},
