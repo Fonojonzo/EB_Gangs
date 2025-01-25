db7295e2231290342558833537b55d0bbaa79c65
iExTno <132175289+iExTno@users.noreply.github.com>
2025-01-25 20:33:42 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -262,7 +262,7 @@ local Gangs = {
 	},----------------------------------- Gang ---------------------------------------------
 
 	[34427514] = {   -- [Dono ID:4592829229 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡 / raio parcelado 11/11 84 reais(HP AUMENTADO)
-		Nome = "Renegados das Trevas",
+		Nome = "Os kizukis",
 		Vida = 1000,
 		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0,['FireKick'] = 0,['Raio'] = 0},
 		Roupa = {Tier = 255,Shirt = 15808598116,Pants = 15815194659},
