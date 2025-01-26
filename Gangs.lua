b731e0c1cc958113ba1e3abbb2b59c7608f0b4d8
iExTno <132175289+iExTno@users.noreply.github.com>
2025-01-25 21:18:24 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -267,7 +267,7 @@ local Gangs = {
 		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0,['FireKick'] = 0,['Raio'] = 0},
 		Roupa = {Tier = 255,Shirt = 15808598116,Pants = 15815194659},
 		Cor = { 
-			Nome = 	  {Letra = {0, 0, 0},Borda = 	   {255, 255, 102}},
+			Nome = 	  {Letra = {80, 0, 0},Borda = 	   {0, 0, 0}},
 			Patente = {Letra = {255, 255, 255},Borda = {255, 255, 102}},
 			Team =    {Letra = {255, 255, 102},Borda = {255, 255, 255}},
 		},
