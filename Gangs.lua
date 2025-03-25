692cc2584cd217529f0f08f1d3f8cdf64d708fdf
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-03-24 21:43:47 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1179,7 +1179,7 @@ local Gangs = {
 	[35621371] = {   -- [Dono ID: 1303845169 ] (HP AUMENTADO) / MAX 10 
 		Nome = "The New Sun 🉐️🌀",
 		Vida = 1000,
-		Itens = {['Josef'] = 0,['YinYang'] = 0},
+		Itens = {['Josef'] = 0,['YinYang'] = 0,['SusanooSlap'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 255},Borda = {0, 0, 0}},
