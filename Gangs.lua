3aa71681fc9c65c557d2f844fcc63b18634720b3
iExTno <132175289+iExTno@users.noreply.github.com>
2025-03-30 05:47:07 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -849,7 +849,7 @@ local Gangs = {
 		Nome = "Os Hashiras",
 		Vida = 1000,
 		Itens = {['Mikey'] = 0},
-		Roupa = {Tier = 256,Shirt = 12793861717,Pants = 12793919614},
+		Roupa = {Tier = 256,Shirt = 94864207126135,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
 			Patente = {Letra = {0, 0, 0},Borda = {4, 0, 122}},
