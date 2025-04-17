64b6b6c330f48f1ddf9fdba6b0ddf3dfda3b4f44
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-04-17 15:37:28 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -249,7 +249,7 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[0] = {   -- [Dono ID:4592829229 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡 / raio parcelado 11/11 84 reais(HP AUMENTADO) / 34427514 venda de cargo dnv
+	[34427514] = {   -- [Dono ID:4592829229 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡 / raio parcelado 11/11 84 reais(HP AUMENTADO) /  venda de cargo dnv
 		Nome = "Os kizukis",
 		Vida = 1000,
 		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0,['FireKick'] = 0,['Raio'] = 0,['MundoTransparente'] = 0},
