46f186387b03d8f13618ad1bc50b03dc4f2d6d54
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-04-21 17:38:46 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -78,7 +78,7 @@ local Gangs = {
 
 	},----------------------------------- Gang ---------------------------------------------
 
-	[0] = {  -- [Dono ID:2623179010 = 3022604859 ] slot 10  /  troca de dono / tentou trocar gang desativada por raça / 33524892
+	[33524892] = {  -- [Dono ID:2623179010 = 3022604859 ] slot 10  /  troca de dono / tentou trocar gang desativada por raça / 
 		Nome = 'Máfia Italiana', 
 		Vida = 2000,
 		Itens = {['Mikey'] = 0,['ChuteCongelante'] = 0,['SusanooSlap'] = 0},
