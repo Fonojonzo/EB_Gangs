c10939f5f40f2271631e22dc21adfa8926c42de3
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-05-19 16:58:19 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1019,7 +1019,7 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[35078814] = {   -- [Dono ID: 2380558325 ] (HP AUMENTADO) / MAX 10 /  venda cargos
+	[0] = {   -- [Dono ID: 2380558325 ] (HP AUMENTADO) / MAX 10 /  venda cargos / 35078814 limite de membro
 		Nome = "----- Death's Curse -----",
 		Vida = 1500,
 		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['Rasteira'] = 0},
