e4162ef7155641bb718825611984ee4be0de8994
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-07-08 12:26:24 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -262,7 +262,7 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[34219212] = {   -- [Dono ID:1630311008 ] slot 15(HP AUMENTADO)
+	[0] = {   -- [Dono ID:1630311008 ] slot 15(HP AUMENTADO) / troca de donos 34219212
 		Nome = "Kanto Manji†",
 		Vida = 1500,
 		Itens = {['KokusenKick'] = 0,['SusanooSlap'] = 0},
