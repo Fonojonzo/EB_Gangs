d97f898bf06e5207657bd433eb6c16dbef8f932d
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-08-11 00:13:35 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1644,9 +1644,9 @@ local Gangs = {
 		Itens = {['Mikey'] = 0,['Dash'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
-			Nome = 	  {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
-			Patente = {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
-			Team =    {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
+			Nome = 	  {Letra = {0, 0, 0},  Borda = {0, 0, 0}},
+			Patente = {Letra = {255, 255, 255},  Borda = {255, 255, 255}},
+			Team =    {Letra = {0, 0, 0},  Borda = {0, 0, 0}},
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
