4b7439a948fd8e05f9495eb1635923427e75b9e0
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-09-09 16:43:25 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1335,8 +1335,8 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[35595693] = {   -- [Dono ID: 7856828708 ] (HP AUMENTADO) / MAX 10 
-		Nome = 'カ Kanto Manji カ',
+	[35595693] = {   -- [Dono ID: 7856828708 ] (HP AUMENTADO) / MAX 10  san
+		Nome = 'Goten',
 		Vida = 2000,
 		Itens = {['Mikey'] = 0,['SusanooSlap'] = 0,['FireKick'] = 0,['Rinnegan_Push'] = {Size = 35},['Colateral'] = 0},
 		Roupa = {Tier = 256,Shirt = 7274942977,Pants = 7276234664},
@@ -1655,6 +1655,7 @@ local Gangs = {
 }
 
 return Gangs
+
 
 
 
