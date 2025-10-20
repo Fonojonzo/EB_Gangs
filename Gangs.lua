6368cabe31ac5bfc646b986b16106f9f97bdb2f3
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-10-20 16:30:55 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -755,7 +755,7 @@ local Gangs = {
 		Nome = "Moebius New Geration",
 		Vida = 1000,
 		Itens = {['SwordSlash'] = 0,['Colateral'] = 0,['Rinnegan_Push'] = {Size = 30},['Raio'] = 0,['SusanooSlap'] = {Color = {160, 120, 89}},
-		Roupa = {Tier = 256,Shirt = 12793861695,Pants = 12793919575},
+		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
 			Patente = {Letra = {0, 0, 0},Borda = {4, 0, 122}},
@@ -1633,6 +1633,7 @@ local Gangs = {
 }
 
 return Gangs
+
 
 
 
