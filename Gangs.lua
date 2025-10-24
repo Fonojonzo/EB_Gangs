e1d3fdbeebd75f99be9cb243c43f61520f9a4db8
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-10-23 23:39:20 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1488,7 +1488,7 @@ local Gangs = {
 			Nome = "SoloXv",
 			Vida = 3000,
 			Itens = {['FireKick'] = 0,['Dash'] = 0,['SusanooSlap'] = {Knockback = 65},['Raio'] = 0,['Rinnegan_Push'] = 0,['YinYang'] = 0},
-			Roupa = {Tier = 256,Shirt = 5933383402,Pants = 5115954271},
+			Roupa = {Tier = 256,Shirt = 13062982935,Pants = 18386095441},
 			Cor = { 
 				Nome = 	  {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
 				Patente = {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
@@ -1633,6 +1633,7 @@ local Gangs = {
 }
 
 return Gangs
+
 
 
 
