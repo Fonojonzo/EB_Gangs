c5f1971008dd80d78f200ac9fa13741cee81a744
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-11-12 16:32:48 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -254,9 +254,9 @@ local Gangs = {
 		},
 	},----------------------------------- Gang ---------------------------------------------
 
-	[34427514] = {   -- [Dono ID:4592829229 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡 / raio parcelado 11/11 84 reais(HP AUMENTADO) /  venda de cargo dnv
+	[34427514] = {   -- [Dono ID:2468759752 ] slot 15 /  ID GANG/ matheusprogamer_123 vendendo cargo -- 34008520 Odyssey Klaw's II 乡 / raio parcelado 11/11 84 reais(HP AUMENTADO) /  venda de cargo dnv
 		Nome = "Eclipse Syndicate",
-		Dono = 4592829229,
+		Dono = 2468759752,
 		Vida = 1000,
 		Itens = {['KokusenKick'] = 0,['Dash'] = 0,['TacoLucille'] = 0,['Rasteira'] = 0,['FireKick'] = 0,['Raio'] = 0,['MundoTransparente'] = 0,['SusanooSlap'] = 0},
 		Roupa = {Tier = 255,Shirt = 15808598116,Pants = 15815194659},
@@ -1837,6 +1837,7 @@ local Gangs = {
 }
 
 return Gangs
+
 
 
 
