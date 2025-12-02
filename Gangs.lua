63ebe779de7ed66bff9c19b6ff761d7506f129dd
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-12-02 18:41:11 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1328,7 +1328,7 @@ local Gangs = {
 		Nome = '传说"dark legends"传说',
 		Dono = 4123916333,
 		Vida = 1000,
-		Itens = {['Mikey'] = 0},
+		Itens = {['KokusenKick'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {0, 255, 255}},
@@ -1895,6 +1895,7 @@ local Gangs = {
 }
 
 return Gangs
+
 
 
 
