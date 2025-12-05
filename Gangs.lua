f420e6f9fb3224db5cf58b136d7921ceeb630889
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-12-05 19:45:30 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -812,7 +812,7 @@ local Gangs = {
 		Nome = "Goten",
 		Dono = 5551458487,
 		Vida = 1000,
-		Itens = {['SwordSlash'] = 0,['Colateral'] = 0,['Rinnegan_Push'] = {Size = 30},['Raio'] = 0,['SusanooSlap'] = {Color = {160, 120, 89}},['Hinokami_1'] = 0},
+		Itens = {['SwordSlash'] = 0,['Colateral'] = 0,['Rinnegan_Push'] = {Size = 30},['Raio'] = 0,['SusanooSlap'] = {Color = {160, 120, 89}},['Hinokami_1'] = 0,['ImpulsoSombrio'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},Borda = {255, 255, 255}},
@@ -1924,6 +1924,7 @@ return Gangs
 
 
 	
+
 
 
 
