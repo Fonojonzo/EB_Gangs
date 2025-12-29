cd44946a578bc5df5f5a30a2cee04a3c196e2df0
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-12-28 23:57:33 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1604,9 +1604,9 @@ local Gangs = {
 			Itens = {['FireKick'] = 0,['Dash'] = 0,['SusanooSlap'] = {Knockback = 65,Color = {0,0,0}},['Raio'] = 0,['Rinnegan_Push'] = 0,['YinYang'] = 0,['Colateral'] = 0},
 			Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 			Cor = { 
-				Nome = 	  {Letra = {0, 0, 0},  Borda = {255, 255, 255}},
-				Patente = {Letra = {0, 0, 0},  Borda = {85, 0, 0}},
-				Team =    {Letra = {0, 0, 0},  Borda = {85, 0, 0}},
+				Nome = 	  {Letra = {0, 0, 0},  Borda = {0, 0, 0}},
+				Patente = {Letra = {0, 0, 0},  Borda = {0, 0, 0}},
+				Team =    {Letra = {0, 0, 0},  Borda = {0, 0, 0}},
 			},
 		},----------------------------------- Gang ---------------------------------------------
 
@@ -1976,6 +1976,7 @@ return Gangs
 
 
 	
+
 
 
 
