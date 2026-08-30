2c9ba688f2eb36f7d7d0b45a80bb399db98266ad
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2026-08-30 12:59:05 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -1613,7 +1613,7 @@ local Gangs = {
 		Nome = "噸~Império The Sombras~噸👑", --  tentando dar golpe no rafael
 		Dono = 4693466988,
 		Vida = 1000,
-		Itens = {['Mikey'] = 0,['Yamato'] = 0,['Raio'] = 0},
+		Itens = {['Mikey'] = 0,['Yamato'] = 0},
 		Roupa = {Tier = 256,Shirt = 0,Pants = 0},
 		Cor = { 
 			Nome = 	  {Letra = {0, 0, 0},  Borda = {153, 0, 204}},
