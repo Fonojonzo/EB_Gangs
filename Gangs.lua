62618743fafdf1215adad47dde052a59715cb9dc
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2026-09-07 12:01:37 -0300

    Update Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -17,7 +17,7 @@ local Gangs = {
 		Nome = 'Akatsuki',
 		Vida = 5000,
 		Efeito = 'FireStroke',
-		Itens = {['KokusenPunch'] = 0,['Rinnegan_Push'] = {Size = 10,Voice = 117779581030667,Debounce = 5,Knockback = 130},['Susanoo2Swords'] = {Color = {0,0,255},Knockback = 75,Debounce = 2.5,Sound = 5686379468}},
+		Itens = {['Colateral'] = 0,['ShurikenExplosiva'] = 0,['KokusenPunch'] = 0,['Rinnegan_Push'] = {Size = 10,Voice = 117779581030667,Debounce = 5,Knockback = 130},['Susanoo2Swords'] = {Color = {0,0,255},Knockback = 75,Debounce = 2.5,Sound = 5686379468}},
 		Roupa = {Tier = 95,Shirt = 0,Pants = 0},
 		Cor = {
 			Nome = 	  {Letra = {0,0,0},Borda = {255,255,255}},
